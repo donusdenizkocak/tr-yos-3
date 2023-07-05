7be34bf59e476342826ee52c1b1b883cc2ef09e7
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-06 00:38:45 +0300

    scrolled

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import CardHome from './components/CardHome';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeNavbar from './components/HomeNavbar';
 
 function App() {
   return (
     <div>
+      <HomeNavbar/>
     <Header/>
     <CardHome/>
     <Footer/>
