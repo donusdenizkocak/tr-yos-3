db6f10e2b8e6bd801473028ccea1ba5f1b07afbe
email <racbernazgul@gmail.com>
2023-07-08 15:20:51 +0300

    nazz

--- src/components/homeComponents/HomeAction.jsx
+++ src/components/homeComponents/HomeAction.jsx
@@ -1,19 +1,20 @@
 const HomeAction = () => {
   return (
-    <div className="border border-collapse bg-[#6398CA] px-80 py-20">
-      <div>
-        <div className="flex-1  justify-center items-center text-white ">
+    <div className="border w-full flex flex-col justify-center items-center border-collapse bg-[#6398CA] py-20">
+
+      
+        <div className="flex flex-col px-10 md:px-0 gap-3 justify-center items-center text-white">
           <h1 className="font-extrabold text-center text-xl">
             Are You Already Working With Us?
           </h1>
-          <p className="text-center text-lg mt-3">
+          <p className="text-center text-lg ">
             At vero eos et accusamus et iusto odio dignissimos ducimus qui
             blanditiis praesentium voluptatum deleniti atque corrupti quos
             dolores et quas molestias
           </p>
         </div>
 
-        <div className="rounded-full border bg-white px-4 pt-4 mt-10">
+        <div className="rounded-full border w-[25rem]   bg-white px-4 pt-4 mt-10">
           <div className="rounded-full relative  mb-4 flex flex-wrap items-stretch">
             <input
               type="text"
@@ -24,7 +25,7 @@ const HomeAction = () => {
             />
 
             <button
-              className="z-[2] inline-block rounded-full bg-darkBlue px-6 pb-2 pt-2.5 mx-3 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              className="w-[120px] h-[52px] z-[2] inline-block rounded-full bg-darkBlue px-6 pb-2 pt-2.5 mx-3 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:z-[3] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
               data-te-ripple-init
               type="button"
               id="button-addon2"
@@ -33,7 +34,8 @@ const HomeAction = () => {
             </button>
           </div>
         </div>
-    </div>
+      </div>
+    
   );
 };
 
