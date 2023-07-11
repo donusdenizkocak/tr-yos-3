84e8431e9835299a9f55af450e4f74f20c338447
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-11 13:44:33 +0300

    a + h4

--- src/components/detailComponents/BasicDetail.jsx
+++ src/components/detailComponents/BasicDetail.jsx
@@ -4,7 +4,16 @@ const BasicDetail = () => {
   return (
     <div className="detail-bloc">
       <div className="detail-bloc_header">
-
+      <a
+          className="basic-control"
+          data-bs-toggle="basic"
+          data-bs-parent="#basicinfo"
+          href="#basicinfo"
+          aria-controls="basicinfo"
+          aria-expanded="false"
+        >
+          <h4 className="block_title">Basic Detail</h4>
+        </a>
 
 
 
