2b07d8441acd8129dc9ac8b4f8f8e8559995e1f2
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-11 13:41:45 +0300

    detail-bloc

--- src/components/detailComponents/BasicDetail.jsx
+++ src/components/detailComponents/BasicDetail.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const BasicDetail = () => {
   return (
-   
+    <div className="detail-bloc">
+      <div className="detail-bloc_header">
+
+
+
+
+      </div>
+      </div>
   )
 }
 
