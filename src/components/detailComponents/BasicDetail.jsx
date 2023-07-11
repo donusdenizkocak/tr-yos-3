267114384bfadcdc6ad8eb771c31d99d08e019b3
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-11 13:46:58 +0300

    basicinfo

--- src/components/detailComponents/BasicDetail.jsx
+++ src/components/detailComponents/BasicDetail.jsx
@@ -14,10 +14,16 @@ const BasicDetail = () => {
         >
           <h4 className="block_title">Basic Detail</h4>
         </a>
-
-
-
       </div>
+      <div id="basicinfo" className="panel-collapse collapse show" aria-labelledby="basicinfo">
+        <div className="detail-bloc-body">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="col-span-1">
+              <div className="ilio-icon-wrap">
+                <div className="ilio-icon">
+                  <i className="fa-solid fa-bed"></i>
+                </div>
+                
       </div>
   )
 }
