d0c257d71f902e7bd5774212862844f694f81f11
Burak ÜSTÜN <118963789+Burakustunnn@users.noreply.github.com>
2023-07-12 16:47:18 +0300

    framer syfa geçişleri

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -3,10 +3,15 @@ import DepartmentsForm from "../components/departmentsComponents/DepartmentsForm
 
 import DeparmentsCard from "../components/departmentsComponents/DeparmentsCard";
 import DepartmentUnvCard from "../components/departmentsComponents/DepartmentUnvCard";
+import { motion } from "framer-motion";
 
 const Departments = () => {
   return (
-    <div className="flex flex-col gap-12 mb-12">
+    <motion.div
+      className="flex flex-col gap-12 mb-12"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1}}
+    >
       <div
         className="h-[240px] w-full "
         style={{
@@ -31,8 +36,8 @@ const Departments = () => {
         </div>
       </div>
 
-      <DepartmentUnvCard/>
-    </div>
+      <DepartmentUnvCard />
+    </motion.div>
   );
 };
 
--- src/pages/Universites.jsx
+++ src/pages/Universites.jsx
@@ -1,9 +1,23 @@
 import CardUniversites from "../components/universitesComponents/CardUniversites";
+import { motion } from "framer-motion";
 
-const mapss = ["a", "b", "c", "d", "e", "f", "g", "h", "i",];
+const mapss = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+
+const framerContainer = {
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+
 const Universites = () => {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1}}
+    >
       <div
         className="h-[240px] w-full "
         style={{
@@ -15,12 +29,17 @@ const Universites = () => {
       >
         <div className="h-full flex flex-col justify-end text-white md:mx-auto md:container pb-8">
           <h2 className=" font-bold text-[50px]">Universites</h2>
-          <p className="text-sm font-medium">Tüm Üniversiteleri Kontrol Edebilirsiniz</p>
+          <p className="text-sm font-medium">
+            Tüm Üniversiteleri Kontrol Edebilirsiniz
+          </p>
         </div>
       </div>
-{mapss.map((map, i) =>( <CardUniversites {...map} key={i} />))}
- 
-  </div>
+      <motion.div initial="hidden" animate="visible" variants={framerContainer}>
+        {mapss.map((map, i) => (
+          <CardUniversites {...map} key={i} />
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
