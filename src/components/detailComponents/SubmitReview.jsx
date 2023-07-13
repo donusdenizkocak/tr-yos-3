039db1eb0f53a55b6ced249610f54f54338e9a3d
denizderK <denizderya8668@gmail.com>
2023-07-13 12:41:56 +0300

    submit-review-son

--- src/components/detailComponents/SubmitReview.jsx
+++ src/components/detailComponents/SubmitReview.jsx
@@ -7,89 +7,102 @@ export const SubmitReview = () => {
   <form>
     {/*E-mail input*/}
     <div className='flex'>
-    <div className="relative mb-6" data-te-input-wrapper-init="">
+    <div className=" mb-6 me-3 flex-1" data-te-input-wrapper-init="">
+    <label
+        htmlFor="exampleInputEmail1"
+        className="pointer-events-none  mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-slate-600 transition-all duration-200 ease-out peer-focus:-translate-y-[1.2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-slate-700 dark:peer-focus:text-primary"
+      >
+        Email address
+      </label>
       <input
         type="text"
-        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+        className="peer block min-h-[auto] w-full rounded border-2  bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-50 data-[te-input-state-active]:placeholder:opacity-50 motion-reduce:transition-none text-gray-500 dark:placeholder:text-neutral-500 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 focus:border-blue-500"
         id="exampleInputEmail1"
         aria-describedby="emailHelp"
         placeholder="Enter email"
       />
-      <label
-        htmlFor="exampleInputEmail1"
-        className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-      >
-        Email address
-      </label>
+    
+      
     </div>
      {/*Password input*/}
-     <div className="relative mb-6" data-te-input-wrapper-init="">
-      <input
-        type="email"
-        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-        id="exampleInputPassword2"
-        placeholder="Password"
-      />
-      <label
+     <div className=" mb-6 flex-1" data-te-input-wrapper-init="">
+     <label
         htmlFor="exampleInputPassword2"
-        className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+        className="pointer-events-none  mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-slate-700 dark:peer-focus:text-primary"
       >
         Email
       </label>
+      <input
+        type="email"
+        className="peer block min-h-[auto] w-full rounded border-2 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-50 data-[te-input-state-active]:placeholder:opacity-50 motion-reduce:transition-none text-gray-500 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 focus:border-blue-500"
+        id="exampleInputPassword2"
+        placeholder="Password"
+      />
+     
     </div>
 
     </div>
-    <div className='flex'>
-    <div className="relative mb-6" data-te-input-wrapper-init="">
+    <div className='flex '>
+    <div className=" mb-6 me-3 flex-1" data-te-input-wrapper-init="">
+    <label
+        htmlFor="exampleInputEmail3"
+        className="pointer-events-none  mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-slate-700 dark:peer-focus:text-primary"
+      >
+        Phone No
+      </label>
       <input
         type="email"
-        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+        className="peer block min-h-[auto] w-full rounded border-2 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-50 data-[te-input-state-active]:placeholder:opacity-50 motion-reduce:transition-none text-gray-500 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 focus:border-blue-500"
         id="exampleInputEmail3"
         aria-describedby="emailHelp"
         placeholder="Your Contact"
       />
-      <label
-        htmlFor="exampleInputEmail3"
-        className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-      >
-        Phone No
-      </label>
+     
     </div>
      {/*Password input*/}
-     <div className="relative mb-6" data-te-input-wrapper-init="">
-      <input
-        type="password"
-        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-        id="exampleInputPassword4"
-        placeholder="Password"
-      />
-      <label
-        htmlFor="exampleInputPassword4"
-        className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+     <div className=" mb-6 flex-1" data-te-input-wrapper-init="">
+    
+     <label
+        htmlFor="exampleInputEmail3"
+        className="pointer-events-none  mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-slate-700 dark:peer-focus:text-primary"
       >
-        Password
+       Ratting
       </label>
+  <select
+    id="exampleInputEmail3"
+    className="bg-transparent     focus:ring-slate-300 focus:border-blue-500     dark:placeholder-gray-400 dark:focus:ring-slate-200 dark:focus:border-blue-500
+    
+    peer block min-h-[auto] w-full rounded border-2  px-3 py-[0.55rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-50 data-[te-input-state-active]:placeholder:opacity-50 motion-reduce:transition-none text-gray-500 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+    
+    
+  >
+    <option selected="" className='p-2'>1: Very Poor</option>
+    <option value="US" className='p-2'>2: Poor</option>
+    <option value="CA" className='p-2'>3: Good</option>
+    <option value="FR" className='p-2'>4: Very Good</option>
+    <option value="DE" className='p-2'>5: Super</option>
+  </select>
+    </div>
     </div>
 
-    </div>
-    <div>
+   
          {/*Message textarea*/}
-  <div className="relative mb-6" data-te-input-wrapper-init="">
-    <textarea
-      className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-      id="exampleFormControlTextarea13"
-      rows={3}
-      placeholder="Messages"
-      defaultValue={""}
-    />
-    <label
-      htmlFor="exampleFormControlTextarea13"
-      className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-800 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-    >
-     Your Message
-    </label>
-  </div>
-    </div>
+      <div className=" mb-6" data-te-input-wrapper-init="">
+          <label
+              htmlFor="exampleFormControlTextarea13"
+              className="pointer-events-none  mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-800 transition-all duration-200 ease-out peer-focus:-translate-y-[1.2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-slate-700 dark:peer-focus:text-primary"
+            >
+            Your Message
+            </label>
+            <textarea
+              className="peer block min-h-[auto] w-full rounded border-2 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-50 data-[te-input-state-active]:placeholder:opacity-50 motion-reduce:transition-none text-gray-500  dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 focus:border-blue-500"
+              id="exampleFormControlTextarea13"
+              rows={3}
+              placeholder="Messages"
+              defaultValue={""}
+            />     
+      </div>
+   
     
    
    
@@ -106,9 +119,6 @@ export const SubmitReview = () => {
     </div>
   </form>
 </div>
-
-
-
 
   )
 }
