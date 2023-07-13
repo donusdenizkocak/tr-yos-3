57b37aaa7ef023397a5ef2252ea6ff1e3b013ee6
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-13 09:38:32 +0300

    icons name changes

--- src/components/detailComponents/BasicDetail.jsx
+++ src/components/detailComponents/BasicDetail.jsx
@@ -8,9 +8,9 @@ const BasicDetail = () => {
   };
   return (
     <div>
-      <div className=" justify-between p-2 border mt-8 border-gray-100 ">
+      <div className=" justify-between p-2 mt-8 ">
         <div className="flex space-x-96 justify-end">
-          <h3 className="  ">Basic Detail</h3>
+          <h3 className=" text-darkBlue text-lg ">Basic Detail</h3>
           <div onClick={handleToggle}>
             <Icon name="arrowUp" size="25" color="darkblue" />
           </div>
@@ -19,72 +19,27 @@ const BasicDetail = () => {
 
       {show && (
         <div className="flex p-10 mb-6 relative  rounded-md  bg-transparent text-blue-900 underline  ">
-          <ul className="flex flex-wrap grid-cols-2 p-8 w-full justify-around" >
+          <ul className="flex flex-wrap grid-cols-2 p-8 w-full justify-around">
             <li className=" p-20 bg-gray-200">
-              <Icon name="bedIcon" size="25" color="darkblue" />
-            </li>
-            <li className=" p-20 bg-gray-200" >
-              <Icon name="groupIcon" size="25" color="darkblue" />
+              <Icon name="bed" size="50" color="darkblue" />
+              <p>3</p>
             </li>
             <li className=" p-20 bg-gray-200">
-              <Icon name="warehouseIcon" size="25" color="darkblue" />
+              <Icon name="group" size="50" color="darkblue" />
+              <p>4,240</p>
+            </li>
+            <li className=" p-20 bg-gray-200">
+              <Icon name="warehouse" size="50" color="darkblue" />
+              <p>1</p>
+            </li>
+            <li className=" p-20 bg-gray-200">
+              <Icon name="buildingCircleCheck" size="50" color="darkblue" />
+              <p>Active</p>
             </li>
           </ul>
         </div>
       )}
     </div>
-    // <div className="relative block w-full mb-6 bg-white p-0 rounded-md">
-    //   <div className="p-4 border-none">
-    //   <a
-    //       className="text-blue-900 underline bg-transparent"
-    //       data-bs-toggle="collapse"
-    //       data-bs-parent="#basicinfo"
-    //       href="#basicinfo"
-    //       aria-controls="basicinfo"
-    //       aria-expanded="false"
-    //     >
-    //       <h4 className="property-block_title">Basic Detail</h4>
-    //     </a>
-    //   </div>
-    //   <div id="basicinfo" className="panel-collapse collapse show" aria-labelledby="basicinfo">
-    //     <div className="detail-bloc-body">
-    //       <div className="grid grid-cols-2 gap-4">
-    //         <div className="col-span-1">
-    //           <div className="ilio-icon-wrap">
-    //             <div className="ilio-icon">
-    //              <basic
-    //             </div>
-    //             <div className="ilio-text">3</div>
-    //           </div>
-    //         </div>
-    //         <div className="col-span-1">
-    //           <div className="ilio-icon-wrap">
-    //             <div className="ilio-icon">
-    //               <i className="fa-solid fa-layer-group"></i>
-    //             </div>
-    //             <div className="ilio-text">4,240</div>
-    //           </div>
-    //         </div>
-    //         <div className="col-span-1">
-    //           <div className="ilio-icon-wrap">
-    //             <div className="ilio-icon">
-    //               <i className="fa-solid fa-warehouse"></i>
-    //             </div>
-    //             <div className="ilio-text">1</div>
-    //           </div>
-    //         </div>
-    //         <div className="col-span-1">
-    //           <div className="ilio-icon-wrap">
-    //             <div className="ilio-icon">
-    //               <i className="fa-solid fa-building-circle-check"></i>
-    //             </div>
-    //             <div className="ilio-text">Active</div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   </div>
   );
 };
 
