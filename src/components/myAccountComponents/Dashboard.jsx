3c462be568327fc6f6db0277a36261d984b69fcf
email <racbernazgul@gmail.com>
2023-07-20 15:46:54 +0300

    myaccount

--- src/components/myAccountComponents/Dashboard.jsx
+++ src/components/myAccountComponents/Dashboard.jsx
@@ -102,25 +102,6 @@ const Dashboard = () => {
               className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
             ></label>
           </div>
-          <div className="text-xl text-darkBlue font-bold mb-2 mx-3">
-            <h3>Social Accounts</h3>
-          </div>
-
-          <div className="border relative mb-3" data-te-input-wrapper-init>
-            <input
-              type="url"
-              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleFormControlInputURL"
-              placeholder="Example label"
-            />
-            <label
-              htmlFor="exampleFormControlInputURL"
-              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >
-              https://facebook.com/
-            </label>
-          </div>
-
           {/*Submit button*/}
           <button
             type="submit"
