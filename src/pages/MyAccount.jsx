2d2e22044a727082c5da126c01207361ef1226bb
Jinsuerer <h.jinsuerer@icloud.com>
2023-07-22 15:55:28 +0200

    22.07.2023-profil-jin

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Dashboard from '../components/myAccountComponents/Dashboard'
+import Profil from '../components/myAccountComponents/Profil'
 
 const MyAccount = () => {
   return (
-    <div>
+    <div className='flex justify-center items-center gap-9'>
+      <Profil/>
       <Dashboard/>
     </div>
   )
