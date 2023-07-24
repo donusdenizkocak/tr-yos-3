2c1c4c8a5096d716f27bc9223e125185b091f4cd
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-24 21:55:16 +0300

    changes in this comparison

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -104,6 +104,66 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
+<<<<<<< Updated upstream
+=======
+
+
+      <div
+        className="invisible fixed left-0 right-0 top-0 z-[1045] flex h-1/3 max-h-full max-w-full -translate-y-full flex-col border-none bg-white bg-clip-padding text-neutral-700 shadow-sm outline-none transition duration-300 ease-in-out  [&[data-te-offcanvas-show]]:transform-none"
+        tabIndex={-1}
+        id="offcanvasTop"
+        aria-labelledby="offcanvasTopLabel"
+        data-te-offcanvas-init
+      >
+        <div className="flex items-center justify-between p-4 ">
+          {/* <h5
+            className="mb-0 font-semibold leading-normal"
+            id="offcanvasTopLabel"
+          >
+            Offcanvas top
+          </h5> */}
+
+          <ul className=" h-full flex flex-col justify-center  ">
+
+        <li className=" mb-0 p-2 font-semibold leading-normal">
+          <a href="/" className=" no-underline text-darkBlue text-xl hover:text-blue-700" >HomePage</a>
+        </li>
+        <li className=" mb-0 p-2 font-semibold leading-normal">
+          <a href="/" className=" no-underline text-darkBlue text-xl hover:text-blue-700" >Universites</a>
+        </li>
+        <li className=" mb-0 p-2 font-semibold leading-normal">
+          <a href="/" className=" no-underline text-darkBlue text-xl hover:text-blue-700">Departments</a>
+        </li>
+      </ul>
+
+
+
+          <button
+            type="button"
+            className=" my-1 box-content rounded-none border-none opacity-50 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+            data-te-offcanvas-dismiss
+          >
+            <span className="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>
+
+>>>>>>> Stashed changes
     </>
   );
 };
