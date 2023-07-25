c83cf06c406b03b2e955ab41bd2fdc128f94db8f
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-26 02:14:10 +0300

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,9 +97,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-
-
-
       <div
         className="invisible fixed left-0 right-0 top-0 z-[1045] flex h-1/3 max-h-full max-w-full -translate-y-full flex-col border-none bg-white bg-clip-padding text-neutral-700 shadow-sm outline-none transition duration-300 ease-in-out  [&[data-te-offcanvas-show]]:transform-none"
         tabIndex={-1}
@@ -108,28 +105,32 @@ const Navbar = () => {
         data-te-offcanvas-init
       >
         <div className="flex items-center justify-between p-4 ">
-          {/* <h5
-            className="mb-0 font-semibold leading-normal"
-            id="offcanvasTopLabel"
-          >
-            Offcanvas top
-          </h5> */}
-
           <ul className=" h-full flex flex-col justify-center  ">
-
-        <li className=" mb-0 p-2 font-semibold leading-normal">
-          <a href="/" className=" no-underline text-darkBlue text-xl hover:text-blue-700" >HomePage</a>
-        </li>
-        <li className=" mb-0 p-2 font-semibold leading-normal">
-          <a href="/" className=" no-underline text-darkBlue text-xl hover:text-blue-700" >Universites</a>
-        </li>
-        <li className=" mb-0 p-2 font-semibold leading-normal">
-          <a href="/" className=" no-underline text-darkBlue text-xl hover:text-blue-700">Departments</a>
-        </li>
-      </ul>
-
-
-
+            <li className=" mb-0 p-2 font-semibold leading-normal">
+              <a
+                href="/"
+                className=" no-underline text-darkBlue text-xl hover:text-blue-700"
+              >
+                HomePage
+              </a>
+            </li>
+            <li className=" mb-0 p-2 font-semibold leading-normal">
+              <a
+                href="/"
+                className=" no-underline text-darkBlue text-xl hover:text-blue-700"
+              >
+                Universites
+              </a>
+            </li>
+            <li className=" mb-0 p-2 font-semibold leading-normal">
+              <a
+                href="/"
+                className=" no-underline text-darkBlue text-xl hover:text-blue-700"
+              >
+                Departments
+              </a>
+            </li>
+          </ul>
           <button
             type="button"
             className=" my-1 box-content rounded-none border-none opacity-50 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
@@ -154,8 +155,6 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-
-
     </>
   );
 };
