225e913c01e5b65482a04d32bd73c9ffae18ddfc
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-07-26 16:28:11 +0300

    remove

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import HomeContextProvider from "./context/HomeContext";
 import AuthContextProvider from "./context/AuthContext";
-import MyAccountForm from "./components/myAccountComponents/MyAccountForm";
+
 function App() { 
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <HomeContextProvider>
-          <AppRouter />  
-          <MyAccountForm/>       
+          <AppRouter />        
         </HomeContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
